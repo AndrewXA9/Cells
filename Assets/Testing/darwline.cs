5a8b57b7800e7ebc3de165a6669541923e4fc118
Andrew <andrewxa9@yahoo.com>
2015-06-10 13:08:31 -0700

    initial push

---
+++ Assets/Testing/darwline.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vectrosity;
+
+public class darwline : MonoBehaviour {
+
+	private VectorLine myLine;
+	
+	void Start () {
+		myLine = VectorLine.SetLine (Color.green, RandomPoint(), RandomPoint());
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			myLine.points2.Add (RandomPoint());
+			myLine.Draw();
+		}
+	}
+	
+	
+	Vector2 RandomPoint() {
+		return new Vector2(Random.Range(0, Screen.width), Random.Range(0, Screen.height));
+	}
+	
+}
